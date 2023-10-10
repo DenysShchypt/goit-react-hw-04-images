8f6eb9a7798d5add07909bc808b5a12906070730
DenysShchypt <denysshchyptsovit@gmail.com>
2023-10-10 21:09:16 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react'
+import { SearchBar } from './SearchBar/SearchBar'
+import { ImageGallery } from './ImageGallery/ImageGallery'
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem'
+import { Loader } from './Loader/Loader'
+import { Button } from './Button/Button'
+import { Modal } from './Modal/Modal'
+
+export class App extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <>
+        <SearchBar></SearchBar>
+        <ImageGallery></ImageGallery>
+        <ImageGalleryItem></ImageGalleryItem>
+        <Loader></Loader>
+        <Button></Button>
+        <Modal></Modal>
+      </>
+    )
+  }
+}
+
+export default App
+
